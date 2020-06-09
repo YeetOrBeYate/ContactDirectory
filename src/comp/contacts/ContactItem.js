c21098dfd82fcb35bc734b5b5ddecb0a37748d65
Kyle Corbin <kylecorbin26@gmail.com>
2020-06-08 21:26:24 -0400

    Working on making a theme for buttons, I think i'm getting the hang of things

--- src/comp/contacts/ContactItem.js
+++ src/comp/contacts/ContactItem.js
@@ -1,19 +1,36 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
+import { palette } from '@material-ui/system';
 import Card from '@material-ui/core/Card';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 import Chip from '@material-ui/core/Chip';
+import { lightGreen, purple } from '@material-ui/core/colors';
 
 const ContactItem = ({contact}) => {
 
-    const useStyles = makeStyles({
+    const useStyles = makeStyles((theme)=>({
         root:{
-            minWidth:300
-        }
-    })
+            minWidth:340
+        },
+        titlehold:{
+            display:'flex',
+            justifyContent:'space-between'
+        },
+        body:{
+            textAlign:'left',
+        },
+        edit:{
+            color:theme.palette.getContrastText(lightGreen[700]),
+            backgroundColor:lightGreen[700],
+            '&:hover':{
+                backgroundColor:lightGreen[900]
+            }
+        },
+
+    }))
 
     const classes = useStyles()
 
@@ -22,12 +39,23 @@ const ContactItem = ({contact}) => {
     return (
         <Card className={classes.root} >
             <CardContent>
-                <div>
-                    <Typography>
+                <div className={classes.titlehold}>
+                    <Typography color='primary' variant='h5'>
                         {name}
                     </Typography>
                     <Chip label={type}/>
                 </div>
+                <Typography className={classes.body} color="info">
+                    {email}
+                </Typography>
+                <Typography className={classes.body} color="secondary">
+                    {phone}
+                </Typography>
+                <CardActions>
+                    <Button variant="contained" className={classes.edit}>Edit</Button>
+                    <Button variant="contained" color="secondary">Delete</Button>
+                </CardActions>
+
             </CardContent>
             
         </Card>
