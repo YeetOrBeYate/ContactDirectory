7959f8c0b46ec7e352c0c73f48dbef7c5d97e625
Kyle Corbin <kylecorbin26@gmail.com>
2020-06-26 19:15:27 -0400

    adjusted the about page

--- src/comp/pages/About.js
+++ src/comp/pages/About.js
@@ -4,7 +4,7 @@ const About = () => {
     return (
         <div>
             <h1>About</h1>
-            <p>This project is just a basic attempt at making a full CRUD application with MongoDB.</p>
+            <p>This project is just a basic attempt at making a full CRUD application with MongoDB. For security purposes I had to hange the password used in MongoDB Atlas, and reconfigure my backend because the password was written in plain text deep in my commits. At the time, I knew I'd need to change it, and I'm glad I did.</p>
         </div>
     )
 }
