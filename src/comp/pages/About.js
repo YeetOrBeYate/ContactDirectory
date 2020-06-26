b6fe4ef4ba24848c8de297bce83980ab37fb2731
Kyle Corbin <kylecorbin26@gmail.com>
2020-06-26 18:33:14 -0400

    Added some filler on the about page

--- src/comp/pages/About.js
+++ src/comp/pages/About.js
@@ -3,7 +3,8 @@ import React from 'react'
 const About = () => {
     return (
         <div>
-            About
+            <h1>About</h1>
+            <p>This project is just a basic attempt at making a full CRUD application with MongoDB.</p>
         </div>
     )
 }
